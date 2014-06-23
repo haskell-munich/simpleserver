8488101b00f0c3bdf778164baaaa9c11c04522b5
Dr. Heinrich Hördegen <hoerdegen@energiefluss.info>
2014-06-23 11:45:15 +0200

    added port flag; added angualarjs

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Happstack.Server
 import qualified Data.ByteString.Lazy.Char8 as B
 
+import System.Environment (getArgs)
 
 import Data.Aeson (decode, encode)
 
@@ -30,12 +31,25 @@ main :: IO ()
 main = do
   d3 <-  B.readFile "d3/d3.v3.min.js"
   jq <-  B.readFile "jquery/jquery-1.11.0.min.js"
+  ang <-  B.readFile "angular/angular.min.js"
 
-  simpleHTTP nullConf $ do
+
+  args <- getArgs
+
+  let p = case args of
+               "-p":n:_ -> read n
+               _ -> 8000
+      conf = nullConf { port = p }
+
+
+
+  simpleHTTP conf $ do
     liftIO (putStrLn "Hi there!")
 
     msum [ dir "d3.v3.min.js" $ ok (setMime "text/javascript" d3),
            dir "jquery-1.11.0.min.js" $ ok (setMime "text/javascript" jq),
+           dir "angular.min.js" $ ok (setMime "text/javascript" ang),
+
            dir "html" $ serveDirectory DisableBrowsing [] "html",
            dir "js" $ serveDirectory DisableBrowsing [] "js",
            dir "css" $ serveDirectory DisableBrowsing [] "css",
